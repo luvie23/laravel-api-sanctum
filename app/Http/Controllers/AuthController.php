b03b95ac42a87c346b75225857cec6de81e3efc3
luvie23 <luvierallos@gmail.com>
2023-06-04 19:47:58 +0800

    practice for api authentication

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Models\User;
+use App\Http\Requests\StoreUserRequest;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    use HttpResponses;
+
+
+    public function login(LoginUserRequest $request) {
+
+        $request->validated($request->all());
+
+        if (!Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+            return $this->error('', 'Credentials do not match', 401);
+        };
+
+        $user = User::where('email', $request->email)->first();
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken($user->name . 'API Token')->plainTextToken
+        ]);
+    }
+
+    public function register(StoreUserRequest $request) {
+
+        $request->validated($request->all());
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        return $this->success([
+            'user' => $user
+        ]);
+    }
+
+    public function logout() {
+        Auth::user()->currentAccessToken()->delete();
+
+        return $this->success('', 'Successfully logged out and token deleted.');
+    }
+}
